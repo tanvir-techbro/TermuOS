825a3bbb7cd56d0892c0f1d31180c64c4e1322c9
RonnieHarrod-cell <ronnieharrod43@gmail.com>
2026-05-21 23:44:06 +0100

    Remove all GUI C++ files

--- kernel/gui/widget.cpp
+++
@@ -1,5 +0,0 @@
-#include "widget.hpp"
-
-// all implementation lives in subclasses (Label, Button, Textbox, List)
-// this file exists so widget.cpp appears in the build and the vtable
-// is emitted here rather than in every translation unit
